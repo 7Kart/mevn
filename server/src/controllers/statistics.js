903f8680a1b6fbb906d084d22a57d9e0605aabba
7Kart <artgor92@gmail.com>
2020-03-26 21:57:52 +0300

    fix

--- server/src/controllers/statistics.js
+++ server/src/controllers/statistics.js
@@ -1,5 +1,6 @@
 const Flats = require("../models/flat"),
     Developer = require("../models/developer"),
+    Types = require("mongoose").Types,
     to = require('await-to-js').default;
 
 
@@ -8,46 +9,61 @@ exports.GetStatisctics = async (req, res) => {
 
     let err = null, flats = null;
 
-
-
-
     const startDate = new Date();
     startDate.setHours(0, 0, 0, 0);
 
     // const dateFinish = new Date(startDate);
     // dateFinish.setDate(dateFinish.getDate() - 1);
 
-
-
-    [err, flats] = await to(Flats.find({
-        projectId: "5e249cc11335fa000067e083",
-        $or: [{
-            $and: [{
-                dateInsert: { $lte: startDate }
-            }, {
-                dtCheck: { $gte: startDate }
-            }]
+    [err, flats] = await to(Flats.aggregate([
+        {
+            $match: {
+                projectId: new Types.ObjectId("5e249cc11335fa000067e083"),
+                dateInsert: { $lte: startDate },
+                changes: { $not: { $size: 0 } },
+                $or: [{
+                    dtCheck: { $gte: startDate }
+                }, {
+                    $and: [{
+                        dtCheck: null
+                    }, {
+                        changes: {
+                            $elemMatch: {
+                                dtChanges: { $gte: startDate }
+                            }
+                        }
+                    }]
+                }]
+            }
         }, {
-            $and: [{
-                dtCheck: null
-            }, {
-                changes: {
-                    $elemMatch: {
-                        
+            $project: {
+                _id: 1,
+                coast: 1,
+                prisePerMeter: 1,
+                dateInsert: 1,
+                dtCheck: 1,
+                changes: 1
+            }
+        }, {
+            $unwind: "$changes"
+        }, {
+            $match: {
+                $and: [
+                    {
+                        "changes.prisePerMeter": { $exists: true }
                     }
-                }
-            }]
-        }]
-    }, {
-        prisePerMeter: 1,
-        dtCheck: 1
-    }));
+                ]
+            }
+        }
+        // }, {
+        //     $limit: 5
+        // }
+    ]));
 
-    console.log(`flats`, flats);
-    console.log(`flats`, flats.length);
+    console.log('err, flats', err, flats, flats.length);
 
-
-    res.json({
-        status: 200
+    res.send({
+        code: 200
     })
+
 }
