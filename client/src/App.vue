daf1679b1c90198e0c37cc05bd79e392f0a7a8b5
7Kart <artgor92@gmail.com>
2020-04-07 00:19:51 +0300

    change layout

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,24 +1,36 @@
 <template>
-  <v-app>
-    <v-card class="overflow-hidden">
-      <v-app-bar
-        :collapse="false"
-        :collapse-on-scroll="false"
-        color="default"
-        dark
-        :dense="true"
-        scroll-target="#scrolling-flats"
-      >
-        <v-app-bar-nav-icon></v-app-bar-nav-icon>
-        <v-toolbar-title>Hives</v-toolbar-title>
-      </v-app-bar>
+  <v-app id="inspire">
+    <v-app-bar :clipped-left="$vuetify.breakpoint.lgAndUp" app color="blue darken-3" collapse-on-scroll="true" dark>
+      <v-toolbar-title style="width: 300px" class="ml-0 pl-3">
+        <v-app-bar-nav-icon @click.stop="mini = !mini"></v-app-bar-nav-icon>
+        <span class="hidden-sm-and-down">Hives</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-app-bar>
 
-      <v-content id="scrolling-flats" class="overflow-y-auto">
-        <v-container>
-          <router-view></router-view>
-        </v-container>
-      </v-content>
-    </v-card>
+    <v-navigation-drawer
+      app
+      :mini-variant.sync="mini"
+      clipped
+      color="grey lighten-4"
+    >
+      <v-list dense class="grey lighten-4">
+        <v-list-item v-for="item in items" :key="item.title" :to="item.href" link>
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-content>
+      <v-container>
+        <router-view></router-view>
+      </v-container>
+    </v-content>
   </v-app>
 </template>
 
@@ -29,7 +41,12 @@ export default {
   components: {},
 
   data: () => ({
-    //
+    drawer: null,
+    mini: true,
+    items: [
+      { title: "Квартиры", icon: "mdi-home-city", href: "/" },
+      { title: "Статистика", icon: "mdi-chart-line", href: "/statistic" }
+    ]
   })
 };
 </script>
