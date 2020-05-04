5e82cf246e35268e349db53371c77908c64928be
7Kart <artgor92@gmail.com>
2020-05-04 18:00:14 +0300

    fix app bar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,36 +1,39 @@
 <template>
   <v-app id="scrolling-flats">
-    <v-app-bar :clipped-left="$vuetify.breakpoint.lgAndUp" app color="blue darken-3" :collapse-on-scroll="true" dark>
-      <v-toolbar-title class="ml-0 pl-3">
-        <v-app-bar-nav-icon @click.stop="mini = !mini"></v-app-bar-nav-icon>
+    <v-app-bar
+      :clipped-left="$vuetify.breakpoint.lgAndUp"
+      app
+      color="blue darken-3"
+      :collapse-on-scroll="true"
+      dark
+    >
+      <v-toolbar-title>
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
         <span class="hidden-sm-and-down">Hives</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
     </v-app-bar>
-
-    <v-navigation-drawer
-      app
-      :mini-variant.sync="mini"
-      clipped
-      color="grey lighten-4"
-    >
-      <v-list dense class="grey lighten-4">
-        <v-list-item v-for="item in items" :key="item.title" :to="item.href" link>
-            <v-list-item-icon>
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title>{{ item.title }}</v-list-item-title>
-            </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
 
     <v-content>
       <v-container fluid>
         <router-view></router-view>
       </v-container>
     </v-content>
+
+    <v-navigation-drawer v-model="drawer" app clipped>
+      <v-list nav dense>
+        <v-list-item-group active-class="light-blue--text text--darken-4">
+          <v-list-item v-for="item in items" :key="item.title" :to="item.href" link>
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
   </v-app>
 </template>
 
@@ -42,12 +45,18 @@ export default {
     this.$store.dispatch("getFlats");
   },
   data: () => ({
-    drawer: null,
+    drawer: false,
     mini: true,
+    group: null,
     items: [
       { title: "Квартиры", icon: "mdi-home-city", href: "/" },
       { title: "Статистика", icon: "mdi-chart-line", href: "/statistic" }
     ]
-  })
+  }),
+  watch: {
+    group() {
+      this.drawer = false;
+    }
+  }
 };
 </script>
